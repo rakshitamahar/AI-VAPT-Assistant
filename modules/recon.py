7c03f0397f8d9e664c4c9103c72a8a5eff74992d
rakshita-mahar <rakshitamahar.rm@gmail.com>
2026-08-14 01:40:38 +0530

    Integrate passive reconnaissance tools

--- modules/recon.py
+++ modules/recon.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 from core.runner import run_command
 
+
 def run_recon(target):
     """
-    Run passive reconnaissance
+    Run passive reconnaissance.
     """
 
     output_dir = Path("outputs")
@@ -13,25 +14,45 @@ def run_recon(target):
 
     logging.info(f"Starting reconnaissance for {target}")
 
-    harvester_result = run_command([
-        "theHarvester",
-        "-d",
-        target,
-        "-b",
-        "crtsh"
-    ])
+    # theHarvester
+    harvester_result = run_command(
+        [
+            "theHarvester",
+            "-d",
+            target,
+            "-b",
+            "crtsh"
+        ],
+        timeout=180
+    )
 
     if harvester_result:
-        with open(output_dir / "harvester.txt", "w", encoding="utf-8") as file:
+        with open(
+            output_dir / "harvester.txt",
+            "w",
+            encoding="utf-8"
+        ) as file:
             file.write(harvester_result.stdout)
 
-    gau_result = run_command([
-        "gau",
-        target
-    ])
+    # GAU
+    gau_result = run_command(
+        [
+            "gau",
+            "--providers",
+            "otx",
+            "--timeout",
+            "15",
+            target
+        ],
+        timeout=60
+    )
 
     if gau_result:
-        with open(output_dir / "gau.txt", "w", encoding="utf-8") as file:
+        with open(
+            output_dir / "gau.txt",
+            "w",
+            encoding="utf-8"
+        ) as file:
             file.write(gau_result.stdout)
 
     logging.info("Reconnaissance completed.")
